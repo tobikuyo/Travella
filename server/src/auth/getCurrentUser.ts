a5d1f508d820541dacc04a2b5eb112fe4e9e4496
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-06 01:42:32 +0100

    Minor comment change

--- server/src/auth/getCurrentUser.ts
+++ server/src/auth/getCurrentUser.ts
@@ -2,7 +2,7 @@ import { AppJwtPayload } from 'interfaces/AppJwtPayload';
 import { User } from 'models';
 
 // This should never return null, since the payload would have been verified by the
-// checkAuthorization middleware, which will throw an error if the user provides an
+// checkAuthorization middleware. The middleware will throw an error if the user provides an
 // invalid access token, before this function is even called
 export const getCurrentUser = async (payload: AppJwtPayload): Promise<User> => {
     const { id } = payload;
