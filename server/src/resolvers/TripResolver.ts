0a90ac7e9017ce020dcb76d5005048924a6dfe8e
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-07 23:01:25 +0100

    Mutations to add hotel and attraction to trip

--- server/src/resolvers/TripResolver.ts
+++ server/src/resolvers/TripResolver.ts
@@ -1,11 +1,11 @@
 import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
 import { AppContext } from 'interfaces/AppContext';
-import { Restaurant } from 'models';
 import {
     checkUserAuthorization,
     checkAuthorizedMembers,
     checkTripExists
 } from 'middleware';
+import { Attraction, Hotel, Restaurant } from 'models';
 import { CreateExperienceInput } from 'typeDefs/inputs';
 import { CreateEntityResult, GetTripResult } from 'typeDefs/unions';
 
@@ -41,6 +41,46 @@ export class TripResolver {
             return { id };
         } catch (error) {
             console.error('Create Restaurant Error', error);
+            return { message: error.message };
+        }
+    }
+
+    // Add hotel to trip
+    @Mutation(() => CreateEntityResult)
+    @UseMiddleware(checkTripExists)
+    @UseMiddleware(checkUserAuthorization)
+    @UseMiddleware(checkAuthorizedMembers)
+    async addHotel(
+        @Ctx() { trip }: AppContext,
+        @Arg('input') hotelInput: CreateExperienceInput,
+        @Arg('invitedUserEmail', { nullable: true }) _invitedUserEmail?: string
+    ): Promise<typeof CreateEntityResult> {
+        try {
+            const hotel = await Hotel.insert({ ...hotelInput, trip });
+            const { id } = hotel.identifiers[0];
+            return { id };
+        } catch (error) {
+            console.error('Create Hotel Error', error);
+            return { message: error.message };
+        }
+    }
+
+    // Add attraction to trip
+    @Mutation(() => CreateEntityResult)
+    @UseMiddleware(checkTripExists)
+    @UseMiddleware(checkUserAuthorization)
+    @UseMiddleware(checkAuthorizedMembers)
+    async addAttraction(
+        @Ctx() { trip }: AppContext,
+        @Arg('input') attractionInput: CreateExperienceInput,
+        @Arg('invitedUserEmail', { nullable: true }) _invitedUserEmail?: string
+    ): Promise<typeof CreateEntityResult> {
+        try {
+            const attraction = await Attraction.insert({ ...attractionInput, trip });
+            const { id } = attraction.identifiers[0];
+            return { id };
+        } catch (error) {
+            console.error('Create Attraction Error', error);
             return { message: error.message };
         }
     }
