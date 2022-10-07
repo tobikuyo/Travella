63d83d0c07adca0163aced2ddac35ff692656b46
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-07 20:51:15 +0100

    Middleware that checks if a trip with the provided id exists
    If it does, it is saved in context, for easier access across the project.

---
+++ server/src/middleware/checkTripExists.ts
@@ -0,0 +1,20 @@
+import { MiddlewareFn } from 'type-graphql';
+import { getTrip } from 'helpers/getTrip';
+import { AppContext } from 'interfaces/AppContext';
+
+export const checkTripExists: MiddlewareFn<AppContext> = async (
+    { args, context },
+    next
+) => {
+    try {
+        const { tripId } = args.input ? args.input : args;
+        const trip = await getTrip(tripId);
+        if (!trip) throw new Error(`There is no trip with the id '${tripId}'`);
+        context.trip = trip;
+    } catch (error) {
+        console.error('Trip Exists Error:', error.message);
+        return { message: error.message };
+    }
+
+    return next();
+};
