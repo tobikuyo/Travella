0450d76dbb5e35f294e07ee22e9119ff111e0c70
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-10 23:41:12 +0100

    Add mutations to update and delete a comment

--- server/src/resolvers/CommentResolver.ts
+++ server/src/resolvers/CommentResolver.ts
@@ -1,9 +1,15 @@
 import { AppContext } from 'interfaces/AppContext';
-import { AuthorizedMembers, TripExists, UserAuthorization } from 'middleware';
+import {
+    AuthorizedMembers,
+    EntityCreator,
+    TripExists,
+    UserAuthorization
+} from 'middleware';
 import { Comment } from 'models';
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from 'type-graphql';
+import { DeleteEntityResult } from 'typeDefs';
 import { CreateCommentInput } from 'typeDefs/inputs';
-import { CreateEntityResult } from 'typeDefs/unions';
+import { CreateEntityResult, UpdateEntityResult } from 'typeDefs/unions';
 
 @Resolver()
 export class CommentResolver {
@@ -26,6 +32,42 @@ export class CommentResolver {
         } catch (error) {
             console.error('Create Comment Error', error);
             return { message: error.message };
+        }
+    }
+
+    // Update comment
+    @Mutation(() => UpdateEntityResult)
+    @UseMiddleware(UserAuthorization, EntityCreator)
+    async updateComment(
+        @Arg('id') id: string,
+        @Arg('text') text: string,
+        @Arg('type', { defaultValue: 'comment' }) _type: string
+    ): Promise<typeof UpdateEntityResult> {
+        try {
+            await Comment.update(id, { text });
+            return { success: true, message: 'Comment was updated successfully' };
+        } catch (error) {
+            console.error('Update Comment Error:', error.message);
+            return { success: false, message: error.message };
+        }
+    }
+
+    // Delete comment
+    @Mutation(() => DeleteEntityResult)
+    @UseMiddleware(UserAuthorization, EntityCreator)
+    async deleteComment(
+        @Arg('id') id: string,
+        @Arg('type', { defaultValue: 'comment' }) _type: string
+    ): Promise<DeleteEntityResult> {
+        try {
+            await Comment.delete(id);
+            return {
+                success: true,
+                message: `Comment with id '${id}' was deleted successfully`
+            };
+        } catch (error) {
+            console.error('Delete Comment Error:', error.message);
+            return { success: false, message: error.message };
         }
     }
 }
