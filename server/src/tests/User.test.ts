3d059667f74fb457752880e171584a394bc15f07
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-20 21:57:41 +0100

    Tests to get user details

--- server/src/tests/User.test.ts
+++ server/src/tests/User.test.ts
@@ -4,10 +4,9 @@ import { getMockRes, getMockReq } from '@jest-mock/express';
 import { User } from 'models';
 import { TestDataSource } from 'test-utils/data-source';
 import { graphqlCall } from 'test-utils/graphqlCall';
-import {
-    LoginMutation,
-    RegisterMutation
-} from './mutations/UserMutations';
+import { CreateTripMutation } from './mutations/TripMutations';
+import { LoginMutation, RegisterMutation } from './mutations/UserMutations';
+import { GetCurrentUserQuery, GetUserQuery } from './queries/UserQueries';
 
 const user = {
     name: faker.name.firstName(),
@@ -21,10 +20,18 @@ const tempUser = {
     type: 'Temp'
 };
 
+const tripInput = {
+    destination: faker.address.cityName(),
+    departureDate: faker.date.recent(2).toISOString(),
+    returnDate: faker.date.soon(10).toISOString(),
+    invitees: [tempUser.email]
+};
+
 const req = getMockReq();
 const { res } = getMockRes();
 
 let accessToken: string;
+let tripId: string;
 
 beforeAll(async () => {
     await TestDataSource.initialize();
@@ -71,5 +78,42 @@ describe('User Success', () => {
 
         expect(accessToken).toBeDefined();
         expect(res.cookie).toBeCalled();
+    });
+
+    it('gets details for current user', async () => {
+        const { data } = await graphqlCall({
+            source: GetCurrentUserQuery,
+            context: { req }
+        });
+
+        const { id, name, email, type, createdAt } = data?.getCurrentUser;
+        expect(id).toBeDefined();
+        expect(name).toBe(user.name);
+        expect(email).toBe(user.email);
+        expect(type).toBe('Registered');
+        expect(createdAt).toBeDefined();
+    });
+
+    it('gets details for a user', async () => {
+        // Create trip for 'TripExists' middleware
+        const { data: tripData } = await graphqlCall({
+            source: CreateTripMutation,
+            context: { req },
+            variableValues: { input: tripInput }
+        });
+
+        tripId = tripData?.createTrip?.id;
+        const databaseUser = await User.findOneBy({ email: user.email });
+
+        const { data } = await graphqlCall({
+            source: GetUserQuery,
+            context: { req },
+            variableValues: { input: { userId: databaseUser?.id, tripId } }
+        });
+
+        const { name, email, type } = data?.getUser;
+        expect(name).toBe(user.name);
+        expect(email).toBe(user.email);
+        expect(type).toBe('Registered');
     });
 });
