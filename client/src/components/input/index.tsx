b34bd7c0f5ccf7441dbc7f2495475b2d4dc1477e
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-23 23:58:30 +0100

    Create custom input component
    It uses react-hook-form to render an error label, if there are any errors

---
+++ client/src/components/input/index.tsx
@@ -0,0 +1,26 @@
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
+import { ErrorLabel, StyledInput } from './Input.styles';
+
+type InputProps<T extends FieldValues> = {
+    name: Path<T>;
+    register: UseFormRegister<T>;
+    error: string | undefined;
+} & Omit<React.ComponentProps<'input'>, 'name'>;
+
+const Input = <T extends FieldValues>({
+    type = 'text',
+    name,
+    register,
+    error,
+    ...rest
+}: InputProps<T>) => {
+    return (
+        <>
+            <StyledInput type={type} id={name} {...rest} {...register(name)} />
+            {/* Displays an error label, if there is an error for the input */}
+            {error && <ErrorLabel htmlFor={name}>{error}</ErrorLabel>}
+        </>
+    );
+};
+
+export default Input;
