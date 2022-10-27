4994d24c08add131cd06501a90d0b703995b9457
Tobi Kuyoro <tobikuyo@gmail.com>
2022-10-27 11:49:43 +0100

    Parse and render popular destination in explore route

--- client/src/routes/explore/Explore.styles.ts
+++ client/src/routes/explore/Explore.styles.ts
@@ -9,16 +9,18 @@ export const Container = styled.div`
              rgba(238, 238, 238, 1) 20%,
             rgba(238, 238, 238, 0.4) 40%,
             rgba(238, 238, 238, 0) 60%
-        )
+        );
 `;
 
 export const MainHeading = styled(Heading)`
     font-size: clamp(2.3rem, 6vw, 3.5rem);
 `;
 
-export const SubHeading = styled(Heading)`
-    font-size: clamp(1.8rem, 5.4vw, 2.5rem);
-    padding-top: 4rem;
+export const SubHeading = styled.h2`
+    font-size: clamp(1.8rem, 5.2vw, 2.5rem);
+    text-transform: capitalize;
+    color: var(--color-heading-dark);
+    padding-block: 3rem 4rem;
 `;
 
 export const HeadingText = styled(Text)`
--- client/src/routes/explore/index.tsx
+++ client/src/routes/explore/index.tsx
@@ -1,13 +1,19 @@
-import { ExploreForm } from 'components';
+import { ExploreForm, PopularDestinations } from 'components';
+import { DestinationDetails } from 'interfaces/trip';
+import destinationsJSON from './destinations.json';
 import { Container, HeadingText, MainHeading, SubHeading } from './Explore.styles';
 
 const Explore = () => {
+    const parsedDestinations = JSON.parse(JSON.stringify(destinationsJSON));
+    const destinations = parsedDestinations['destinations'] as DestinationDetails[];
+
     return (
         <Container>
             <MainHeading textBelow>Explore the world</MainHeading>
             <HeadingText>Browse numerous destinations</HeadingText>
             <ExploreForm />
-            <SubHeading>Explore top destinations</SubHeading>
+            <SubHeading>Popular destinations</SubHeading>
+            <PopularDestinations destinations={destinations} />
         </Container>
     );
 };
